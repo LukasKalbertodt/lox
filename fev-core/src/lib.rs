ce50e9ebbbd01e0483df774703e6a0accf345e86
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-06-18 21:23:45 +0200

    Add `Explicit{Vertex, Face}` traits to core

--- fev-core/src/lib.rs
+++ fev-core/src/lib.rs
@@ -2,3 +2,43 @@
 
 
 pub mod handle;
+pub mod refs;
+
+use crate::{
+    handle::{DefaultId, FaceHandle, VertexHandle},
+    refs::{FaceRef, VertexRef},
+};
+
+// Alternative names:
+// - HasVertices
+// - ExplicitVertex
+// - ContainsVertices
+// - VertexIndex
+// - WithVerts
+pub trait ExplicitVertex {
+    type VertexProp;
+
+    fn vertex_prop(&self, handle: VertexHandle) -> Option<&Self::VertexProp>;
+    fn vertex_prop_mut(&mut self, handle: VertexHandle) -> Option<&mut Self::VertexProp>;
+
+    fn num_vertices(&self) -> DefaultId;
+
+    fn vertices<'s>(&'s self) -> Box<Iterator<Item = VertexRef<Self>> + 's>
+    where
+        Self: Sized;
+    // TODO: visit_mut
+}
+
+pub trait ExplicitFace {
+    type FaceProp;
+
+    fn face_prop(&self, handle: FaceHandle) -> Option<&Self::FaceProp>;
+    fn face_prop_mut(&mut self, handle: FaceHandle) -> Option<&mut Self::FaceProp>;
+
+    fn num_faces(&self) -> DefaultId;
+
+    fn faces<'s>(&'s self) -> Box<Iterator<Item = FaceRef<Self>> + 's>
+    where
+        Self: Sized;
+    // TODO: visit_mut
+}
