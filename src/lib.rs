f2dee7e52bede1e49050cacae46c4da70b6f679b
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-09-16 14:02:46 +0200

    Add `mesh` module

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,10 @@
 
 pub mod handle;
 pub mod math;
+mod mesh;
 pub mod refs;
 
 pub use self::{
     handle::{EdgeHandle, FaceHandle, VertexHandle},
+    mesh::{Mesh, TriMesh, MeshElement, ExplicitFace, ExplicitVertex},
 };
