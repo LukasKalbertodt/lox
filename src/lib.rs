38a472d6a6b3d6c929acbfa135c0bf0b5afbc1a5
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-09-20 12:58:42 +0200

    Add more documentation to the `mesh!` macro

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,72 @@ pub use lox_macros::mesh as mesh;
 ///
 /// # Examples
 ///
+/// Here we create two triangles:
+///
+/// ```
+/// #![feature(proc_macro_non_items)]
+/// # use lox_macros::mesh;
+/// use lox::ds::SharedVertexMesh;
+///
+///
+/// let (mesh, positions, distances, face_colors) = mesh! {
+///     type: SharedVertexMesh,
+///     vertices: [
+///         v0: ([0.0, 0.0, 0.0], 0.0),
+///         v1: ([0.0, 1.0, 0.0], 1.0),
+///         v2: ([1.0, 0.0, 0.0], 1.0),
+///         v3: ([1.0, 1.0, 0.0], 1.414),
+///     ],
+///     faces: [
+///         [v0, v2, v1]: ("red"),
+///         [v3, v1, v2]: ("green"),
+///     ],
+/// };
+/// ```
+///
+/// In the code above, we associate a position and a scalar value with each
+/// vertex and a color (or rather, a color name) with each face. Properties of
+/// vertices and faces are specified after a colon (`:`) in parenthesis (like a
+/// tuple).
+///
+/// For each property you add in those parenthesis, the macro returns an
+/// additional property map. The full return value is:
+///
+/// ```ignore
+/// (mesh, /* vertex property maps */, /* face property maps*/)
+/// ```
+///
+/// ## Without properties
+///
+/// We don't need to specify any properties. We can either write empty
+/// parenthesis (`()`) or just omit the colon and the parenthesis:
+///
+/// ```
+/// #![feature(proc_macro_non_items)]
+/// # use lox_macros::mesh;
+/// use lox::ds::SharedVertexMesh;
+///
+///
+/// let mesh = mesh! {
+///     type: SharedVertexMesh,
+///     vertices: [
+///         v0: (),  // <-- this is equivalent to:
+///         v1,      // <-- this
+///         v2,
+///         v3,
+///     ],
+///     faces: [
+///         [v0, v2, v1],
+///         [v3, v1, v2],
+///     ],
+/// };
+/// ```
+///
+/// Of course, you can also add properties to the vertices, but not the faces,
+/// or the other way around. However, you always have to specify the same
+/// number of properties for all vertices and the same number of properties for
+/// all faces.
+///
 /// ## An empty mesh
 ///
 /// This is not particularly useful in itself, but it works. You can use this
