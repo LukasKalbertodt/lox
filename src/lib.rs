ed93b94b91d73b94c0f9d6a633953b9681330e48
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-09-20 12:44:52 +0200

    Add documentation for `mesh!` macro

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 //! **TODO**: Everything.
 
 #![feature(never_type)]
+#![feature(doc_cfg)]
 
 
 pub mod ds;
@@ -17,4 +18,46 @@ pub use self::{
     handle::{EdgeHandle, FaceHandle, VertexHandle},
     mesh::{Mesh, TriMesh, MeshElement, ExplicitFace, ExplicitVertex},
 };
-pub use lox_macros::mesh;
+
+
+// ===========================================================================
+// ===== Macros
+// ===========================================================================
+
+// Sadly, rustdoc is a bit buggy when it comes to reexporting proc macros. So
+// when rustdoc runs, we use a dummy macro. When the crate is compiled as
+// usual, we will reexport the proc macro.
+#[cfg(not(rustdoc))]
+pub use lox_macros::mesh as mesh;
+
+/// Convenience macro to quickly create a small mesh.
+///
+/// **Note about unstable features**: this proc macro needs to be invoked in
+/// expression context, which is still unstable. So your crate need to enable
+/// the `proc_macro_non_items` feature for this to work.
+///
+/// # Examples
+///
+/// ## An empty mesh
+///
+/// This is not particularly useful in itself, but it works. You can use this
+/// syntax when you haven't yet decided how your mesh should look like.
+///
+/// ```
+/// #![feature(proc_macro_non_items)]
+/// # use lox_macros::mesh;
+/// use lox::ds::SharedVertexMesh;
+///
+///
+/// let empty_mesh = mesh! {
+///     type: SharedVertexMesh,
+///     vertices: [],
+///     faces: [],
+/// };
+/// ```
+#[cfg(rustdoc)]
+#[macro_export]
+macro_rules! mesh {
+    // The real implementation is in `lox_macros` and it's reexported above.
+    (/* proc macro */) => ( /* proc macro */ )
+}
