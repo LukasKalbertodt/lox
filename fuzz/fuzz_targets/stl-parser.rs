7e69b0c928ede794292ada60e9f58610a62ad5a5
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-10-17 12:53:06 +0200

    Add fuzzing directory with one stl parser fuzz test

---
+++ fuzz/fuzz_targets/stl-parser.rs
@@ -0,0 +1,15 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use lox::io::stl;
+
+fuzz_target!(|data: &[u8]| {
+    // We just pass the data to the parsing function and ignore all parsed
+    // data. We are just interested in panics or other even worse crashes. So
+    // we also ignore the returned `Result` as it's fine if the parser says
+    // "this is not a valid STL file".
+    let _ = stl::Reader::new(data).read_raw(&mut stl::AdhocSink {
+        num_triangles: |_| {},
+        add_triangle: |_| {},
+    });
+});
