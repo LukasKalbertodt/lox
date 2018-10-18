7960c035d78defe86b37942a579a1b40fadacbce
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-10-18 15:11:19 +0200

    Update STL fuzzer

--- fuzz/fuzz_targets/stl-parser.rs
+++ fuzz/fuzz_targets/stl-parser.rs
@@ -1,15 +1,12 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use lox::io::stl;
+use lox::io::stl::{Reader, CounterSink};
 
 fuzz_target!(|data: &[u8]| {
     // We just pass the data to the parsing function and ignore all parsed
     // data. We are just interested in panics or other even worse crashes. So
     // we also ignore the returned `Result` as it's fine if the parser says
     // "this is not a valid STL file".
-    let _ = stl::Reader::new(data).read_raw(&mut stl::AdhocSink {
-        num_triangles: |_| {},
-        add_triangle: |_| {},
-    });
+    let _ = Reader::new(data).read_raw_into(&mut CounterSink::new());
 });
