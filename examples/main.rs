6c9f866d3bbfed754b7708bf5ae5d9a2dffff0a3
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-11-08 16:53:22 +0100

    Update main example to test shape API

--- examples/main.rs
+++ examples/main.rs
@@ -5,57 +5,38 @@ use cgmath::{Point3, Vector3};
 use failure::Error;
 
 use lox::{
-    mesh,
+    MeshWithProps,
     ds::SharedVertexMesh,
     io::{stl, ply},
     map::{ConstMap, FnMap},
+    mesh,
     prelude::*,
+    shape,
 };
 
 
 fn main() -> Result<(), Error> {
-    // let (mesh, positions, _face_normals) = mesh! {
-    //     type: SharedVertexMesh,
-    //     vertices: [
-    //         v0: (Point3::new(0.0f32, 0.0, 0.0)),
-    //         v1: (Point3::new(0.0, 1.0, 0.0)),
-    //         v2: (Point3::new(1.0, 0.0, 0.0)),
-    //         v3: (Point3::new(1.0, 1.0, 1.0)),
-    //     ],
-    //     faces: [
-    //         [v0, v2, v1]: (Vector3::new(0.0f32, 0.0, 1.0)),
-    //         [v3, v1, v2]: (Vector3::new(0.0, 0.0, -1.0)),
-    //     ],
-    // };
+    // let disc = shape::Disc {};
+    // disc.build();
 
-    // ply::Serializer::ascii()
-    // stl::Serializer::binary()
-    //     .into_writer(&mesh, &positions)
-    //     // .add_vertex_prop("peter", &ConstMap(27u8))
-    //     // .with_vertex_normals(&ConstMap(Vector3::new(0.0, 0.0, 1.0)))
-    //     // .with_face_normals(&face_normals)
-    //     // .write_to_stdout()?;
-    //     .write_to_file("mesh.stl")?;
+    // let res = gl::Buffer::build_from(shape::Disc {
+    //     faces: 6,
+    // });
+    // println!("{:?}", res);
 
-    let filename = std::env::args().nth(1).unwrap();
-    println!("loading {}", filename);
+    let res = MeshWithProps::<SharedVertexMesh, _, _>::build_from(shape::Disc {
+        // faces: 6,
+        .. Default::default()
+    }).unwrap();
+    // println!("{:#?}", res);
+    ply::Serializer::ascii()
+        .into_writer(
+            &res.mesh,
+            &res.vertex_props.map_value(|prop| prop.position.map(|s| s as f32)),
+        )
+        .with_vertex_normals(&res.vertex_props.map_value(|prop| prop.normal.map(|s| s as f32)))
+        .write_to_file("shape.ply")?;
 
-    let before = std::time::Instant::now();
-    let res = stl::Reader::open(filename)?
-        .read::<SharedVertexMesh>(stl::ReadOptions {
-            unify_vertices: true,
-            read_positions: false,
-            .. stl::ReadOptions::default()
-        })?;
-    println!("Mesh: {} vertices, {} faces", res.mesh.num_vertices(), res.mesh.num_faces());
-    if let Some(positions) = &res.vertex_positions {
-        println!("First position: {:?}", positions[VertexHandle::from_usize(0)]);
-    }
-    if let Some(normals) = &res.face_normals {
-        println!("First normal: {:?}", normals[FaceHandle::from_usize(0)]);
-    }
-
-    println!("elapsed: {:.2?}", before.elapsed());
 
     Ok(())
 }
