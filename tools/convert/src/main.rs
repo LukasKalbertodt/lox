9cf443b8dd702c06226793f3d1052f28a74341f6
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-12-07 13:24:34 +0100

    Add `AnyPointMap` and use new `StreamingSource` API in `convert`

--- tools/convert/src/main.rs
+++ tools/convert/src/main.rs
@@ -10,9 +10,10 @@ use failure::{err_msg, Error, ResultExt};
 use structopt::StructOpt;
 
 use lox::{
-    MeshWithProps,
+    cgmath::Point3,
     ds::SharedVertexMesh,
-    io::{FileFormat, stl, ply},
+    map::VecMap,
+    io::{FileFormat, StreamingSource, MemSink, Primitive, PrimitiveType, stl, ply},
     prelude::*,
 };
 
@@ -46,14 +47,16 @@ fn run() -> Result<(), Error> {
     println!("{:?}", opt);
 
     // Load file
-    load_file(&opt).context("could not read source file")?;
+    let mesh_data = load_file(&opt).context("could not read source file")?;
+
+    println!("{:#?}", mesh_data);
 
     Ok(())
 }
 
-fn load_file(opt: &Opt) -> Result<(), Error> {
+fn load_file(opt: &Opt) -> Result<MeshData, Error> {
     let mut file = File::open(&opt.source).context("failed to open file")?;
-    let file_start = {
+    let _file_start = {
         let mut v = Vec::new();
         file.by_ref().take(1024).read_to_end(&mut v)?;
         file.seek(SeekFrom::Start(0))?;
@@ -70,16 +73,131 @@ fn load_file(opt: &Opt) -> Result<(), Error> {
 
     println!("source format: {:?}", file_format);
 
+    let mut mesh_data = MeshData::new();
     match file_format {
         FileFormat::Ply => {
-            let _reader = ply::Reader::new(file).context("failed to read PLY header")?;
+            let reader = ply::Reader::new(file).context("failed to read PLY header")?;
+            reader.transfer_to(&mut mesh_data).context("failed to read PLY body")?;
 
         }
         FileFormat::Stl => {
-            let _reader = stl::Reader::new(file).context("failed to read STL header")?;
-
+            let reader = stl::Reader::new(file).context("failed to read STL header")?;
+            reader.transfer_to(&mut mesh_data).context("failed to read STL body")?;
         }
     }
 
-    Ok(())
+    Ok(mesh_data)
+}
+
+
+#[derive(Debug)]
+struct MeshData {
+    mesh: SharedVertexMesh,
+    vertex_positions: Option<AnyPointMap<VertexHandle>>,
+}
+
+impl MeshData {
+    fn new() -> Self {
+        Self {
+            mesh: SharedVertexMesh::new(),
+            vertex_positions: None,
+        }
+    }
+}
+
+impl MemSink for MeshData {
+    fn add_vertex(&mut self) -> VertexHandle {
+        self.mesh.add_vertex()
+    }
+    fn add_face(&mut self, vertices: [VertexHandle; 3]) -> FaceHandle {
+        self.mesh.add_face(vertices)
+    }
+
+    fn set_vertex_position<N: Primitive>(
+        &mut self,
+        handle: VertexHandle,
+        position: Point3<N>,
+    ) {
+        self.vertex_positions
+            .get_or_insert_with(|| AnyPointMap::new::<N>())
+            .insert(handle, position);
+    }
+}
+
+#[derive(Debug)]
+enum AnyPointMap<H: Handle> {
+    Uint8(VecMap<H, Point3<u8>>),
+    Int8(VecMap<H, Point3<i8>>),
+    Uint16(VecMap<H, Point3<u16>>),
+    Int16(VecMap<H, Point3<i16>>),
+    Uint32(VecMap<H, Point3<u32>>),
+    Int32(VecMap<H, Point3<i32>>),
+    Float32(VecMap<H, Point3<f32>>),
+    Float64(VecMap<H, Point3<f64>>),
+}
+
+impl<H: Handle> AnyPointMap<H> {
+    fn new<T: Primitive>() -> Self {
+        match T::TY {
+            PrimitiveType::Uint8 => AnyPointMap::Uint8(VecMap::new()),
+            PrimitiveType::Int8 => AnyPointMap::Int8(VecMap::new()),
+            PrimitiveType::Uint16 => AnyPointMap::Uint16(VecMap::new()),
+            PrimitiveType::Int16 => AnyPointMap::Int16(VecMap::new()),
+            PrimitiveType::Uint32 => AnyPointMap::Uint32(VecMap::new()),
+            PrimitiveType::Int32 => AnyPointMap::Int32(VecMap::new()),
+            PrimitiveType::Float32 => AnyPointMap::Float32(VecMap::new()),
+            PrimitiveType::Float64 => AnyPointMap::Float64(VecMap::new()),
+        }
+    }
+
+    fn primitive_type(&self) -> PrimitiveType {
+        match self {
+            AnyPointMap::Uint8(_) => PrimitiveType::Uint8,
+            AnyPointMap::Int8(_) => PrimitiveType::Int8,
+            AnyPointMap::Uint16(_) => PrimitiveType::Uint16,
+            AnyPointMap::Int16(_) => PrimitiveType::Int16,
+            AnyPointMap::Uint32(_) => PrimitiveType::Uint32,
+            AnyPointMap::Int32(_) => PrimitiveType::Int32,
+            AnyPointMap::Float32(_) => PrimitiveType::Float32,
+            AnyPointMap::Float64(_) => PrimitiveType::Float64,
+        }
+    }
+
+    fn insert<T: Primitive>(&mut self, handle: H, pos: Point3<T>) {
+        // This function optimizes very well! As seen [here][godbolt], it is
+        // just one check of our discriminant against a constant. If the check
+        // fails, jump to panic, otherwise the value is just pushed to the
+        // vector. Optimal, so to speak!
+        //
+        // [godbolt]: https://rust.godbolt.org/z/dyWROg
+
+        macro_rules! insert {
+            ($map:ident, $convert:ident) => {{
+                $map.insert(handle, pos.map(|s| s.value().$convert().unwrap()));
+            }}
+        }
+
+        // Make sure the inserted type matches the type of the map
+        if T::TY != self.primitive_type() {
+            panic!(
+                "type mismatch inserting '{:?}' into a AnyPointMap with type '{:?}'",
+                T::TY,
+                self.primitive_type(),
+            )
+        }
+
+        // Since we know here that the types match, all those `to_*` convert
+        // functions won't ever return `None`. In fact, the compiler can
+        // probably prove that since it has static type information about `T`.
+        match self {
+            AnyPointMap::Uint8(map) => insert!(map, to_u8),
+            AnyPointMap::Int8(map) => insert!(map, to_i8),
+            AnyPointMap::Uint16(map) => insert!(map, to_u16),
+            AnyPointMap::Int16(map) => insert!(map, to_i16),
+            AnyPointMap::Uint32(map) => insert!(map, to_u32),
+            AnyPointMap::Int32(map) => insert!(map, to_i32),
+            AnyPointMap::Float32(map) => insert!(map, to_f32),
+            AnyPointMap::Float64(map) => insert!(map, to_f64),
+        }
+    }
 }
