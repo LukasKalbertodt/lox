e003ed68a880b78148a98c56cefce7373de26fa7
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-12-10 21:42:35 +0100

    Add two simple PLY reading tests

--- src/io/ply/tests.rs
+++ src/io/ply/tests.rs
@@ -1,4 +1,5 @@
 use failure::Error;
+use smallvec::SmallVec;
 
 use crate::{
     mesh,
@@ -6,9 +7,15 @@ use crate::{
     ds::SharedVertexMesh,
     map::{ConstMap, FnMap, VecMap},
 };
-use super::{Serializer, Encoding};
+use super::{
+    Serializer, Encoding, Reader, PropertyType, ScalarType, PropIndex, Property,
+    ListLenType, RawOffset, RawResult,
+};
 
 
+// ===========================================================================
+// ===== Writing
+// ===========================================================================
 
 fn triangle_mesh() -> (SharedVertexMesh, VecMap<VertexHandle, [f32; 3]>) {
     mesh! {*
@@ -157,5 +164,80 @@ fn write_triangle_with_extra_props_ble() -> Result<(), Error> {
         .write_to_memory()?;
 
     assert_eq_file!(&res, "triangle_with_extra_props_ble.ply");
+    Ok(())
+}
+
+
+// ===========================================================================
+// ===== Reading
+// ===========================================================================
+
+fn check_triangle(res: &RawResult) {
+    let groups = &res.element_groups;
+    assert_eq!(groups.len(), 2);
+
+    let g0 = &groups[0];
+    assert_eq!(g0.def.name, "vertex");
+    assert_eq!(g0.def.count, 3);
+    assert_eq!(g0.def.property_defs[PropIndex(0)].ty, PropertyType::Scalar(ScalarType::Float));
+    assert_eq!(g0.def.property_defs[PropIndex(0)].name, "x");
+    assert_eq!(g0.def.property_defs[PropIndex(1)].ty, PropertyType::Scalar(ScalarType::Float));
+    assert_eq!(g0.def.property_defs[PropIndex(1)].name, "y");
+    assert_eq!(g0.def.property_defs[PropIndex(2)].ty, PropertyType::Scalar(ScalarType::Float));
+    assert_eq!(g0.def.property_defs[PropIndex(2)].name, "z");
+
+    assert_eq!(g0.elements[0].prop_infos[PropIndex(0)].offset, RawOffset(0));
+    assert_eq!(g0.elements[0].prop_infos[PropIndex(1)].offset, RawOffset(4));
+    assert_eq!(g0.elements[0].prop_infos[PropIndex(2)].offset, RawOffset(8));
+    assert_eq!(g0.elements[0].iter().collect::<Vec<_>>(), &[
+        Property::Float(0.0),
+        Property::Float(0.0),
+        Property::Float(0.0),
+    ]);
+    println!("{:?}", g0.elements[1].data);
+    println!("{:#?}", g0.elements[1].prop_infos);
+    assert_eq!(g0.elements[1].iter().collect::<Vec<_>>(), &[
+        Property::Float(3.0),
+        Property::Float(5.0),
+        Property::Float(8.0),
+    ]);
+    assert_eq!(g0.elements[2].iter().collect::<Vec<_>>(), &[
+        Property::Float(1.942),
+        Property::Float(152.99),
+        Property::Float(0.007),
+    ]);
+
+    let g1 = &groups[1];
+    assert_eq!(g1.def.name, "face");
+    assert_eq!(g1.def.count, 1);
+    assert_eq!(g1.def.property_defs[PropIndex(0)].ty, PropertyType::List {
+        len_type: ListLenType::UChar,
+        scalar_type: ScalarType::UInt,
+    });
+    assert_eq!(g1.def.property_defs[PropIndex(0)].name, "vertex_indices");
+
+    assert_eq!(
+        g1.elements[0].iter().collect::<Vec<_>>(),
+        &[Property::UIntList(SmallVec::from_vec(vec![0, 1, 2]))],
+    );
+}
+
+#[test]
+fn read_raw_triangle_bbe() -> Result<(), Error> {
+    let input = include_test_file!("triangle_bbe.ply");
+    let res = Reader::new(input)?.into_raw_result()?;
+
+    check_triangle(&res);
+
+    Ok(())
+}
+
+#[test]
+fn read_raw_triangle_ascii() -> Result<(), Error> {
+    let input = include_test_file!("triangle_ascii.ply");
+    let res = Reader::new(input)?.into_raw_result()?;
+
+    check_triangle(&res);
+
     Ok(())
 }
