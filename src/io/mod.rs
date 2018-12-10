0195be8799539ab41e526fc57840668bb8daa244
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-12-10 19:57:29 +0100

    Add documentation and do some cleanup in `io`

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -66,6 +66,7 @@ pub trait MeshWriter {
     }
 }
 
+/// Represents one of the supported file formats.
 #[derive(Debug, Clone, Copy)]
 pub enum FileFormat {
     Ply,
@@ -73,6 +74,12 @@ pub enum FileFormat {
 }
 
 impl FileFormat {
+    /// Tries to guess the file format from the file extension.
+    ///
+    /// Returns `None` if:
+    /// - the path/file has no extension in its name, or
+    /// - the extension is no valid UTF8, or
+    /// - the file extension is not known.
     pub fn from_extension(path: impl AsRef<Path>) -> Option<Self> {
         path.as_ref()
             .extension()
@@ -87,6 +94,15 @@ impl FileFormat {
     }
 }
 
+// ==========================================================================
+// ===== Primitives
+// ==========================================================================
+
+/// Represents the type of an IO primitive.
+///
+/// This is closely related to [`PrimitiveValue`] (which represents an IO
+/// primitive value) and [`Primitive`] (which is a trait abstracting over the
+/// closed set of IO primitive types).
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum PrimitiveType {
     Uint8,
@@ -99,6 +115,11 @@ pub enum PrimitiveType {
     Float64,
 }
 
+/// Represents an IO primitive value.
+///
+/// This is closely related to [`PrimitiveType`] (which represents only the
+/// type of IO primitives) and [`Primitive`] (which is a trait abstracting over
+/// the closed set of IO primitive types).
 #[derive(Debug, Clone, Copy)]
 pub enum PrimitiveValue {
     Uint8(u8),
@@ -113,43 +134,86 @@ pub enum PrimitiveValue {
 
 macro_rules! make_convert_method {
     ($name:ident, $ty:ident, $variant:ident) => {
+        make_convert_method!($name, $ty, $variant, stringify!($ty), stringify!($variant));
+    };
+    ($name:ident, $ty:ident, $variant:ident, $ty_str:expr, $variant_str:expr) => {
+        /// Returns this value as `
+        #[doc = $ty_str]
+        /// ` if `self` is `
+        #[doc = $variant_str]
+        /// `. Otherwise, `None` is returned. This function does not cast
+        /// between different number types!
         pub fn $name(&self) -> Option<$ty> {
             match self {
                 PrimitiveValue::$variant(x) => Some(*x),
                 _ => None,
             }
         }
-    }
+    };
 }
 
 impl PrimitiveValue {
-    make_convert_method!(to_u8, u8, Uint8);
-    make_convert_method!(to_i8, i8, Int8);
-    make_convert_method!(to_u16, u16, Uint16);
-    make_convert_method!(to_i16, i16, Int16);
-    make_convert_method!(to_u32, u32, Uint32);
-    make_convert_method!(to_i32, i32, Int32);
-    make_convert_method!(to_f32, f32, Float32);
-    make_convert_method!(to_f64, f64, Float64);
+    make_convert_method!(as_u8, u8, Uint8);
+    make_convert_method!(as_i8, i8, Int8);
+    make_convert_method!(as_u16, u16, Uint16);
+    make_convert_method!(as_i16, i16, Int16);
+    make_convert_method!(as_u32, u32, Uint32);
+    make_convert_method!(as_i32, i32, Int32);
+    make_convert_method!(as_f32, f32, Float32);
+    make_convert_method!(as_f64, f64, Float64);
+
+    /// Returns the type of this value.
+    pub fn ty(&self) -> PrimitiveType {
+        match self {
+            PrimitiveValue::Uint8(_) => PrimitiveType::Uint8,
+            PrimitiveValue::Int8(_) => PrimitiveType::Int8,
+            PrimitiveValue::Uint16(_) => PrimitiveType::Uint16,
+            PrimitiveValue::Int16(_) => PrimitiveType::Int16,
+            PrimitiveValue::Uint32(_) => PrimitiveType::Uint32,
+            PrimitiveValue::Int32(_) => PrimitiveType::Int32,
+            PrimitiveValue::Float32(_) => PrimitiveType::Float32,
+            PrimitiveValue::Float64(_) => PrimitiveType::Float64,
+        }
+    }
 }
 
-pub trait Primitive:
-    PrimitiveNum +
-    AsPrimitive<u8> +
-    AsPrimitive<i8> +
-    AsPrimitive<u16> +
-    AsPrimitive<i16> +
-    AsPrimitive<u32> +
-    AsPrimitive<i32> +
-    AsPrimitive<f32> +
-    AsPrimitive<f64>
-{
+mod internal {
+    pub trait DoNotImplement {}
+}
+
+/// Abstracts over all IO primitive types.
+///
+/// Note that this trait is exactly implemented for the types that are included
+/// in [`PrimitiveType`] and [`PrimitiveValue`]. Thus, this is a closed set of
+/// implementing types (unusual for a trait). As a consequence, you are not
+/// supposed to implement this trait for your own types! That's why this trait
+/// has a supertrait called `DoNotImplement`. Said supertrait is crate-private,
+/// so you can't implement it for other types.
+pub trait Primitive: PrimitiveNum + internal::DoNotImplement {
+    /// The type represented as this [`PrimitiveType`] value.
     const TY: PrimitiveType;
+
+    /// Returns the primitive as a [`PrimitiveValue`] (basically dynamic
+    /// typing).
+    ///
+    /// The implementation of this method always returns a value with the same
+    /// type as specified in `Self::TY`. In other words: `T::TY ==
+    /// t.to_primitive_value().ty()` is always true for all primitives `t` with
+    /// type `T`.
     fn to_primitive_value(&self) -> PrimitiveValue;
+
+    /// Returns `Some(self)` if `T == Self`, or `None` otherwise.
+    ///
+    /// This method is useful to convert a generic value into a value of a
+    /// specific type.
+    fn downcast_as<T: Primitive>(self) -> Option<T> {
+        AsPrimitive::as_primitive(self)
+    }
 }
 
 macro_rules! impl_primitive {
     ($ty:ident, $variant:ident) => {
+        impl internal::DoNotImplement for $ty {}
         impl Primitive for $ty {
             const TY: PrimitiveType = PrimitiveType::$variant;
             fn to_primitive_value(&self) -> PrimitiveValue {
@@ -174,6 +238,7 @@ impl_primitive!(i32, Int32);
 impl_primitive!(f32, Float32);
 impl_primitive!(f64, Float64);
 
+/// A helper trait to implement [`Primitive::downcast_as`]. Ignore this.
 pub trait AsPrimitive<Target: Primitive> {
     fn as_primitive(self) -> Option<Target>;
 }
@@ -184,10 +249,10 @@ impl<Source: Primitive, Target: Primitive> AsPrimitive<Target> for Source {
     }
 }
 
-pub fn as_primitive<Target: Primitive, Source: AsPrimitive<Target>>(src: Source) -> Option<Target> {
-    src.as_primitive()
-}
 
+// ==========================================================================
+// ===== {Streaming/Mem}-Sinks and Sources
+// ==========================================================================
 
 pub trait StreamingSource {
     type Error: Fail;
