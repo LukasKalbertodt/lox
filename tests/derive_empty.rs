aaa76bd2237f9e51e298299d526187d228b20f8b
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-02-14 19:39:21 +0100

    Improve `derive(Empty)` test to catch missing full paths

--- tests/derive_empty.rs
+++ tests/derive_empty.rs
@@ -1,36 +1,33 @@
 //! Tests the custom derive for `Empty`.
-
-use lox::{
-    Empty, FaceHandle,
-    ds::SharedVertexMesh,
-    map::VecMap,
-};
+//!
+//! We use complete path everywhere and avoid imports to make sure when the
+//! derive code falsely assumes a symbol is in scope.
 
 
 #[test]
 fn unit_like() {
-    #[derive(Empty)]
+    #[derive(lox::Empty)]
     struct UnitLike;
 
-    let _ = UnitLike::empty();
+    let _ = <UnitLike as lox::Empty>::empty();
 }
 
 
 #[test]
 fn unnamed() {
-    #[derive(Empty)]
+    #[derive(lox::Empty)]
     struct UnnamedEmpty();
 
-    #[derive(Empty)]
-    struct UnnamedSingle(SharedVertexMesh);
+    #[derive(lox::Empty)]
+    struct UnnamedSingle(lox::ds::SharedVertexMesh);
 
-    #[derive(Empty)]
-    struct UnnamedTwo(SharedVertexMesh, VecMap<FaceHandle, u32>);
+    #[derive(lox::Empty)]
+    struct UnnamedTwo(lox::ds::SharedVertexMesh, lox::map::VecMap<lox::FaceHandle, u32>);
 
 
-    let _ = UnnamedEmpty::empty();
-    let _ = UnnamedSingle::empty();
-    let _ = UnnamedTwo::empty();
+    let _ = <UnnamedEmpty as lox::Empty>::empty();
+    let _ = <UnnamedSingle as lox::Empty>::empty();
+    let _ = <UnnamedTwo as lox::Empty>::empty();
 }
 
 
@@ -38,22 +35,22 @@ fn unnamed() {
 fn named() {
     #![allow(dead_code)]
 
-    #[derive(Empty)]
+    #[derive(lox::Empty)]
     struct NamedEmpty {}
 
-    #[derive(Empty)]
+    #[derive(lox::Empty)]
     struct NamedSingle {
-        mesh: SharedVertexMesh,
+        mesh: lox::ds::SharedVertexMesh,
     }
 
-    #[derive(Empty)]
+    #[derive(lox::Empty)]
     struct NamedTwo {
-        mesh: SharedVertexMesh,
-        stuff: VecMap<FaceHandle, u32>,
+        mesh: lox::ds::SharedVertexMesh,
+        stuff: lox::map::VecMap<lox::FaceHandle, u32>,
     }
 
 
-    let _ = NamedEmpty::empty();
-    let _ = NamedSingle::empty();
-    let _ = NamedTwo::empty();
+    let _ = <NamedEmpty as lox::Empty>::empty();
+    let _ = <NamedSingle as lox::Empty>::empty();
+    let _ = <NamedTwo as lox::Empty>::empty();
 }
