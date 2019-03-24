0fd57ee5d22a13c8da1048321910666985015a04
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-03-24 20:57:35 +0100

    Add `NullSinkPos` to PLY benchmark

--- benches/io_ply.rs
+++ benches/io_ply.rs
@@ -4,17 +4,26 @@ use std::io::Cursor;
 use criterion::{
     criterion_group, criterion_main, black_box, BatchSize, Criterion,
 };
+use cgmath::Point3;
 
 use lox::{
+    prelude::*,
+    handle::hsize,
+    VertexHandle, FaceHandle,
     io::{
-        Error,
+        Error, Primitive,
         ply::{
             Reader,
             raw::{ElementDef, RawElement, RawSink},
         },
     },
+    util::MeshSizeHint,
 };
 
+
+// ===============================================================================================
+// ===== Helper utilities
+// ===============================================================================================
 
 /// A raw sink that just puts all data into the `black_box`.
 struct NullRawSink;
@@ -30,6 +39,58 @@ impl RawSink for NullRawSink {
     }
 }
 
+
+/// A sink that puts all vertex positions into the `black_box`, ignores all
+/// other properties.
+struct NullSinkPos {
+    vertex_count: hsize,
+    face_count: hsize,
+}
+
+impl NullSinkPos {
+    fn new() -> Self {
+        Self {
+            vertex_count: 0,
+            face_count: 0,
+        }
+    }
+}
+
+impl MemSink for NullSinkPos {
+    fn add_vertex(&mut self) -> VertexHandle {
+        let out = VertexHandle::new(self.vertex_count);
+        self.vertex_count += 1;
+        out
+    }
+    fn add_face(&mut self, vertices: [VertexHandle; 3]) -> FaceHandle {
+        black_box(vertices);
+
+        let out = FaceHandle::new(self.face_count);
+        self.face_count += 1;
+        out
+    }
+
+    fn size_hint(&mut self, hint: MeshSizeHint) {
+        black_box(hint);
+    }
+
+    fn prepare_vertex_positions<N: Primitive>(&mut self, count: hsize) -> Result<(), Error> {
+        black_box(count);
+        Ok(())
+    }
+    fn set_vertex_position<N: Primitive>(
+        &mut self,
+        v: VertexHandle,
+        position: Point3<N>,
+    ) {
+        black_box(v);
+        black_box(position);
+    }
+}
+
+// ===============================================================================================
+// ===== Benchmarks
+// ===============================================================================================
 
 /// Measures body reading of `three_tris_all_props` files via `RawSink`.
 fn three_tris_all_props_raw(c: &mut Criterion) {
