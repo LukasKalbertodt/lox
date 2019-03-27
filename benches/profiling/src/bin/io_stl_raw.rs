d5c0079bd3156c89329474e3f58a603b48502bef
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-03-27 19:03:37 +0100

    Add STL raw read profiling binary

---
+++ benches/profiling/src/bin/io_stl_raw.rs
@@ -0,0 +1,23 @@
+use std::{
+    fs,
+    io::Cursor,
+};
+
+use criterion::black_box;
+use lox::{
+    io::{
+        stl::Reader,
+    }
+};
+
+fn main() {
+    let data = get_data();
+    let reader = Reader::new(Cursor::new(data)).unwrap();
+    reader.read_raw(|tri| { black_box(tri); }).expect("failed to read");
+}
+
+#[inline(never)]
+fn get_data() -> Vec<u8> {
+    let filename = std::env::args().nth(1).expect("missing source file argument");
+    fs::read(filename).expect("failed to read file")
+}
