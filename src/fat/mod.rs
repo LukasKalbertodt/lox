d67299ea1548876e5ba182427cf6388addea47c4
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-03-30 15:38:17 +0100

    Derive `MemSource` for `fat::MiniMesh` instead of the manual impl

--- src/fat/mod.rs
+++ src/fat/mod.rs
@@ -32,10 +32,9 @@ use cgmath::{Point3, Vector3};
 use crate::{
     self as lox, // for proc macros
     prelude::*,
-    cast,
     ds::SharedVertexMesh,
     handle::hsize,
-    io::{ColorType, Error, ErrorKind, Primitive, PrimitiveType},
+    io::{ColorType, Error, Primitive, PrimitiveType},
     map::VecMap,
 };
 use self::any::{AnyColorMap, AnyPointMap, AnyVectorMap};
@@ -51,34 +50,13 @@ pub mod any;
 /// The type of the core mesh is specified by the type parameter `M`. The cast
 /// rigor for the vertex positions is "lossy", i.e. all kinds of casts are
 /// allowed.
-#[derive(Empty, MemSink, Debug)]
+#[derive(Empty, MemSink, MemSource, Debug)]
 pub struct MiniMesh<M: TriMeshMut + TriVerticesOfFace> {
     #[lox(core_mesh)]
     pub mesh: M,
 
     #[lox(vertex_position, cast = "lossy")]
     pub vertex_positions: VecMap<VertexHandle, Point3<f32>>,
-}
-
-
-impl<M: TriMeshMut + TriVerticesOfFace> MemSource for MiniMesh<M> {
-    type CoreMesh = M;
-    fn core_mesh(&self) -> &Self::CoreMesh {
-        &self.mesh
-    }
-    fn vertex_position_type(&self) -> Option<PrimitiveType> {
-        Some(PrimitiveType::Float32)
-    }
-    fn vertex_position<T: Primitive>(&self, v: VertexHandle) -> Result<Option<Point3<T>>, Error> {
-        if !cast::is_cast_possible::<cast::Lossy, T, f32>() {
-            return Err(Error::new(|| ErrorKind::SourceIncompatible {
-                prop: lox::io::PropKind::VertexPosition,
-                requested_type: T::TY,
-            }));
-        }
-
-        Ok(self.vertex_positions.get(v).map(|p| p.map(|s| cast::lossy(s))))
-    }
 }
 
 
