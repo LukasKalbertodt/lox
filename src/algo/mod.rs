1cdfc255bd91a8a1848f1a20134152536efe0b10
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-04-01 01:35:44 +0200

    Add (unfinished) `is_closed` check

--- src/algo/mod.rs
+++ src/algo/mod.rs
@@ -35,3 +35,33 @@ where
         (v.handle(), new_pos.convert())
     }).collect()
 }
+pub fn is_closed<MeshT>(mesh: &MeshT) -> bool
+where
+    MeshT: Mesh + FacesAroundFace + VerticesAroundFace,
+{
+    // TODO: We can check this property in two ways:
+    // - (a) each edge has two adjacent faces
+    // - (b) each face has the same number of adjacent faces as number of
+    //   adjacent vertices/edges
+    //
+    // If all edges have either 1 or 2 adjacent faces (i.e. no isolated edges
+    // and no fucked-up edges), the two are equivalent, because:
+    // - if (b) => each face has as many edges as vertices. On each edge of the
+    //   face, there can only be one other face. Since there are as many
+    //   adjacent faces as adjacent vertices/eges, each edge has two adjacent
+    //   faces.
+    // - if (a) => if all edges of a face have two adjacent faces, the face has
+    //   as many adjacent faces as edges. Which is also the same number as the
+    //   number of adjacent vertices.
+    //
+    // Problem: So we can perform this check for:
+    // - (a): MeshT: EdgeMesh + FacesOfEdge
+    // - (b): MeshT: Mesh + FacesAroundFace + VerticesAroundFace
+    //      - Note: this is bad already too: if we know it's a triangle mesh,
+    //        we don't need `VerticesAroundFace`
+    //
+    // But we can't have an "or" part in trait bounds. This is meh.
+
+
+    mesh.faces().all(|f| f.adjacent_faces().count() == f.adjacent_vertices().count())
+}
