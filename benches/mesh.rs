19643376e9eab32e6e748ca0ed51b483127c68f3
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-05-31 20:56:58 +0200

    Add vertex-normal benchmark

--- benches/mesh.rs
+++ benches/mesh.rs
@@ -6,12 +6,16 @@ use criterion::{
 
 use lox::{
     prelude::*,
-    cgmath::Point3,
+    cgmath::{
+        Point3, Vector3,
+        prelude::*,
+    },
     ds::{
         DirectedEdgeMesh, HalfEdgeMesh, SharedVertexMesh,
         half_edge::TriConfig,
     },
     fat::MiniMesh,
+    map::VecMap,
     shape::Sphere,
 };
 
@@ -92,11 +96,52 @@ fn count_adjacent_faces(c: &mut Criterion) {
     gen!("hem_count_adjacent_faces", HalfEdgeMesh<TriConfig>);
 }
 
+fn calc_vertex_normals(c: &mut Criterion) {
+    macro_rules! gen {
+        ($name:literal, $mesh:ty) => {
+            c.bench_function($name, |b| {
+                let mesh = MiniMesh::<$mesh>::create_from(MEDIUM_SPHERE).unwrap();
+                let face_normals = mesh.mesh
+                    .face_handles()
+                    .map(|fh| {
+                        let [a, b, c] = mesh.mesh.vertices_around_triangle(fh);
+                        let [pa, pb, pc] = [
+                            mesh.vertex_positions[a],
+                            mesh.vertex_positions[b],
+                            mesh.vertex_positions[c],
+                        ];
+                        let normal = (pb - pa).cross(pc - pa).normalize();
+
+                        (fh, normal)
+                    })
+                    .collect::<VecMap<_, _>>();
+                // println!("{:?}", face_normals.handles().take(10).collect::<Vec<_>>());
+
+                b.iter(|| {
+                    let mesh = black_box(&mesh.mesh);
+
+                    for v in mesh.vertices() {
+                        let normal = v.adjacent_faces()
+                            .map(|f| face_normals[f.handle()])
+                            .sum::<Vector3<f32>>()
+                            .normalize();
+
+                        black_box(normal);
+                    }
+                })
+            });
+        };
+    }
+
+    gen!("dem_calc_vertex_normals", DirectedEdgeMesh);
+    gen!("hem_calc_vertex_normals", HalfEdgeMesh<TriConfig>);
+}
 
 criterion_group!(benches,
     count_vertices,
     count_faces,
     count_adjacent_faces,
+    calc_vertex_normals,
 );
 criterion_main!(benches);
 
