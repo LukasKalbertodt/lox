b370a5a959110a37ba533e8559fc41305841608c
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-08-25 15:57:21 +0200

    Improve and extend `map` test suite

--- src/map/tests.rs
+++ src/map/tests.rs
@@ -1,54 +1,115 @@
+use std::{
+    collections::HashSet,
+    fmt,
+    hash::Hash,
+};
 
+use crate::{
+    prelude::*,
+};
+
+
+pub(crate) fn check<S, T>(left: &mut S, right: &mut [(FaceHandle, T)])
+where
+    S: PropMap<FaceHandle, Target = T> + PropStoreMut<FaceHandle, Output = T>,
+    T: Eq + fmt::Debug + Hash,
+{
+    assert_eq!(left.num_props(), right.len() as u32);
+    assert_eq!(left.is_empty(), right.is_empty());
+
+    macro_rules! assert_iter_eq {
+        ($left:expr, $right:expr) => {{
+            // The order of the iterator is unspecified, so we collect both
+            // into a hash set.
+            let left_set = $left.collect::<HashSet<_>>();
+            let right_set = $right.collect::<HashSet<_>>();
+
+            if left_set != right_set {
+                panic!(
+                    "assert_iter_eq failed!\n  left: `{}` => {:?}\n right: `{}` => {:?}\n",
+                    stringify!($left),
+                    left_set,
+                    stringify!($right),
+                    right_set,
+                );
+            }
+        }};
+    }
+
+    assert_iter_eq!(left.iter(), right.iter().map(|(h, v)| (*h, v)));
+    assert_iter_eq!(left.values(), right.iter().map(|(_, v)| v));
+    assert_iter_eq!(left.handles(), right.iter().map(|(h, _)| *h));
+    assert_iter_eq!(left.iter_mut(), right.iter_mut().map(|(h, v)| (*h, v)));
+    assert_iter_eq!(left.values_mut(), right.iter_mut().map(|(_, v)| v));
+
+    if let Some(max) = right.iter().map(|(h, _)| h.idx()).max() {
+        for idx in 0..max + 2 {
+            let h = FaceHandle::new(idx);
+            if let Some(pos) = right.iter().position(|(h, _)| h.idx() == idx) {
+                assert!(left.contains_handle(h));
+                assert_eq!(left.get_ref(h), Some(&right[pos].1));
+                assert_eq!(&left[h], &right[pos].1);
+                assert_eq!(left.get_mut(h), Some(&mut right[pos].1));
+                assert_eq!(&mut left[h], &mut right[pos].1);
+            } else {
+                assert_eq!(left.get(h), None);
+                assert_eq!(left.get_ref(h), None);
+                assert_eq!(left.get_mut(h), None);
+            }
+        }
+    }
+}
 
 macro_rules! gen_tests_for_store_impl {
     ($name:ident) => {
         use crate::{
             FaceHandle,
-            map::{PropMap, PropStore, PropStoreMut},
+            map::{PropMap, PropStoreMut},
         };
 
+
+        // ----- Helper stuff ------------------------------------------------
         fn h(x: usize) -> FaceHandle {
             FaceHandle::from_usize(x)
         }
 
+        macro_rules! check {
+            ($store:ident, $expected:expr) => {
+                crate::map::tests::check(&mut $store, &mut $expected);
+            };
+        }
+
+
+        // ----- Tests -------------------------------------------------------
+
         #[test]
         fn empty() {
-            let m = $name::<FaceHandle, ()>::empty();
-            assert_eq!(m.num_props(), 0);
+            let mut m = $name::<FaceHandle, ()>::empty();
+            check!(m, []);
+        }
+
+        #[test]
+        fn with_capacity_empty() {
+            let mut m = $name::<FaceHandle, ()>::with_capacity(23);
+            check!(m, []);
         }
 
         #[test]
         fn insert_one() {
             let mut m = $name::empty();
-            let f0 = h(0);
-            m.insert(f0, "a");
-
-            assert_eq!(m.num_props(), 1);
-            assert_eq!(m.get(f0).map(|x| *x), Some("a"));
-            assert_eq!(m.get_ref(f0).map(|x| *x), Some("a"));
-            assert_eq!(m.get_mut(f0).map(|x| *x), Some("a"));
-
-            let f1 = h(1);
-            assert!(m.get(f1).is_none());
-            assert!(m.get_ref(f1).is_none());
-            assert!(m.get_mut(f1).is_none());
-
-            assert_eq!(m.handles().collect::<Vec<_>>(), [f0]);
+            m.insert(h(1), "a");
+            check!(m, [(h(1), "a")]);
         }
 
         #[test]
         fn reinsert() {
             let mut m = $name::empty();
-            let f0 = h(0);
+            let ret0 = m.insert(h(1), "a");
+            let ret1 = m.insert(h(1), "b");
 
-            m.insert(f0, "a");
-            let ret = m.insert(f0, "b");
-
-            assert_eq!(ret, Some("a"));
-            assert_eq!(m.num_props(), 1);
-            assert_eq!(m.get(f0).map(|x| *x), Some("b"));
-            assert_eq!(m.get_ref(f0).map(|x| *x), Some("b"));
-            assert_eq!(m.get_mut(f0).map(|x| *x), Some("b"));
+            assert_eq!(ret0, None);
+            assert_eq!(ret1, Some("a"));
+            check!(m, [(h(1), "b")]);
         }
 
         #[test]
@@ -59,38 +120,21 @@ macro_rules! gen_tests_for_store_impl {
             m.insert(h(0), "x");
             m.insert(h(1), "b");
             m.insert(h(5), "c");
-
-            assert_eq!(m.num_props(), 3);
-            assert_eq!(m.handles().count(), 3);
+            check!(m, [(h(0), "x"), (h(1), "b"), (h(5), "c")]);
 
             m.clear();
-
-            assert_eq!(m.num_props(), 0);
-            assert_eq!(m.handles().count(), 0);
-            assert!(m.get(h(0)).is_none());
-            assert!(m.get(h(0)).is_none());
-            assert!(m.get(h(1)).is_none());
+            check!(m, []);
             assert!(m.get(h(5)).is_none());
 
             m.insert(h(4), "y");
-
-            assert_eq!(m.num_props(), 1);
-            assert_eq!(m.handles().count(), 1);
-            assert!(m.get(h(0)).is_none());
-            assert!(m.get(h(0)).is_none());
-            assert!(m.get(h(1)).is_none());
-            assert!(m.get(h(5)).is_none());
-
-
-            assert_eq!(m.get(h(4)).map(|x| *x), Some("y"));
-            assert_eq!(m.get_ref(h(4)).map(|x| *x), Some("y"));
-            assert_eq!(m.get_mut(h(4)).map(|x| *x), Some("y"));
+            check!(m, [(h(4), "y")]);
         }
 
         #[test]
         fn remove_nothing() {
             let mut m = $name::<_, ()>::empty();
-            assert!(m.remove(h(0)).is_none());
+            assert!(m.remove(h(1)).is_none());
+            check!(m, []);
         }
 
         #[test]
@@ -101,20 +145,64 @@ macro_rules! gen_tests_for_store_impl {
             m.insert(h(1), "b");
             m.insert(h(1), "c");
             m.insert(h(2), "d");
-
-            assert_eq!(m.num_props(), 3);
-            assert_eq!(m.handles().count(), 3);
+            check!(m, [(h(0), "a"), (h(1), "c"), (h(2), "d")]);
 
             let ret = m.remove(h(0));
-
             assert_eq!(ret, Some("a"));
-            assert_eq!(m.num_props(), 2);
-            assert_eq!(m.handles().count(), 2);
+            check!(m, [(h(1), "c"), (h(2), "d")]);
 
             let ret = m.remove(h(1));
             assert_eq!(ret, Some("c"));
-            assert_eq!(m.num_props(), 1);
-            assert_eq!(m.handles().count(), 1);
+            check!(m, [(h(2), "d")]);
+        }
+
+        #[test]
+        fn remove_more_complex() {
+            let mut m = $name::with_capacity(23);
+
+            m.insert(h(0), "a");
+            m.insert(h(1), "b");
+            m.insert(h(1), "c");
+            m.insert(h(2), "d");
+            m.insert(h(3), "e");
+            m.insert(h(4), "f");
+            check!(m, [(h(0), "a"), (h(1), "c"), (h(2), "d"), (h(3), "e"), (h(4), "f")]);
+
+            assert_eq!(m.remove(h(3)), Some("e"));
+            check!(m, [(h(0), "a"), (h(1), "c"), (h(2), "d"), (h(4), "f")]);
+
+            assert_eq!(m.remove(h(1)), Some("c"));
+            check!(m, [(h(0), "a"), (h(2), "d"), (h(4), "f")]);
+
+            assert_eq!(m.remove(h(1)), None);
+            check!(m, [(h(0), "a"), (h(2), "d"), (h(4), "f")]);
+
+            assert_eq!(m.insert(h(3), "x"), None);
+            check!(m, [(h(0), "a"), (h(2), "d"), (h(3), "x"), (h(4), "f")]);
+
+            assert_eq!(m.remove(h(3)), Some("x"));
+            check!(m, [(h(0), "a"), (h(2), "d"), (h(4), "f")]);
+        }
+
+        #[test]
+        fn values_mut() {
+            let mut m = $name::with_capacity(23);
+
+            m.insert(h(0), "a");
+            m.insert(h(1), "b");
+            m.insert(h(2), "c");
+            m.insert(h(3), "d");
+            m.insert(h(4), "e");
+
+            for s in m.values_mut() {
+                match *s {
+                    "b" => *s = "nonono",
+                    "e" => *s = "yes",
+                    _ => (),
+                }
+            }
+
+            check!(m, [(h(0), "a"), (h(1), "nonono"), (h(2), "c"), (h(3), "d"), (h(4), "yes")]);
         }
     }
 }
