ee65aad1d19ad25f1d85d5c3069a12528776ca7e
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-08-30 13:28:41 +0200

    Add more HEM tests to test all configurations of the data structure

--- src/ds/half_edge/tests.rs
+++ src/ds/half_edge/tests.rs
@@ -1,7 +1,15 @@
-mod tri {
-    use super::super::*;
+use crate::traits::marker::{False, True};
 
-    gen_tri_mesh_tests!(HalfEdgeMesh::<TriConfig>: [
+mod tri {
+    use super::{*, super::*};
+
+    pub enum Conf {}
+    impl Config for Conf {
+        type FaceKind = TriFaces;
+        type StorePrev = False;
+    }
+
+    gen_tri_mesh_tests!(HalfEdgeMesh::<Conf>: [
         TriMesh,
         EdgeMesh,
         BasicAdj,
@@ -13,9 +21,55 @@ mod tri {
 }
 
 mod poly {
-    use super::super::*;
+    use super::{*, super::*};
 
-    gen_tri_mesh_tests!(HalfEdgeMesh::<PolyConfig>: [
+    pub enum Conf {}
+    impl Config for Conf {
+        type FaceKind = PolyFaces;
+        type StorePrev = False;
+    }
+
+    gen_tri_mesh_tests!(HalfEdgeMesh::<Conf>: [
+        PolyMesh,
+        EdgeMesh,
+        BasicAdj,
+        FullAdj,
+        EdgeAdj,
+        Manifold,
+        SupportsMultiBlade
+    ]);
+}
+
+mod tri_with_prev {
+    use super::{*, super::*};
+
+    pub enum Conf {}
+    impl Config for Conf {
+        type FaceKind = TriFaces;
+        type StorePrev = True;
+    }
+
+    gen_tri_mesh_tests!(HalfEdgeMesh::<Conf>: [
+        TriMesh,
+        EdgeMesh,
+        BasicAdj,
+        FullAdj,
+        EdgeAdj,
+        Manifold,
+        SupportsMultiBlade
+    ]);
+}
+
+mod poly_with_prev {
+    use super::{*, super::*};
+
+    pub enum Conf {}
+    impl Config for Conf {
+        type FaceKind = PolyFaces;
+        type StorePrev = True;
+    }
+
+    gen_tri_mesh_tests!(HalfEdgeMesh::<Conf>: [
         PolyMesh,
         EdgeMesh,
         BasicAdj,
